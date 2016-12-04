770be62c27792a3333159c8a033a8c238f85b12b
minigonche <minigonche@gmail.com>
2016-12-04 21:59:13 +0000

    Stable version

--- main/bin/app.py
+++ main/bin/app.py
@@ -14,25 +14,18 @@ render = web.template.render('templates/')
 
 class Index(object):
     def GET(self):
-        return render.transformacion(a="1",b="0",c="0",d="1",loc="static/inicial.png")
+        return render.index()
 
 
     def POST(self):
         
-        form = web.input(a="1",b="1j",c="1j",d="1",loc="static/inicial.png", ref = 0)
+        form = web.input(nameradio="1")
         
-        print(form.ref)
+        print(form.nameradio)
         
-        if(form.ref=="1"):
-            return  render.transformacion(a= form.a, b=form.b, c=form.c, d=form.d, loc = "static/inicial.png")
-        else:    
-            matrix = mbt.give_marix_from_string(form.a,form.b,form.c,form.d)
-            print(matrix)
-            mbt.apply_matrix_to_image(matrix,'static/inicial.png','static/trans.png')
-        
-            time.sleep(2)
 
-            return  render.transformacion(a= form.a, b=form.b, c=form.c, d=form.d, loc = "static/trans.png")
+        return  render.respuesta(amigo=form.nameradio)
+     
 
 if __name__ == "__main__":
     app.run()
